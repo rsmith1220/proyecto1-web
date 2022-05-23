5752912af12381c46997f6a06f3f3980bbddf27a
Rebecca Smith <rebecca.smith1220.rs@gmail.com>
2022-05-23 08:44:55 -0600

    comienzo

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss'
+
+
+function App(){
+  return(
+    <div className='todo'>
+      <header>
+        Hello
+      </header>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
